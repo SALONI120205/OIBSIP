fecfae25d2400ab86ad5458cd68a3993fb19a6e7
SALONI120205 <salonigajwe12@gmail.com>
2025-11-27 14:02:03 +0530

    Add files via upload

---
+++ Task2/script.js
@@ -0,0 +1,9 @@
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        target.scrollIntoView({ behavior: "smooth" });
+    });
+});
